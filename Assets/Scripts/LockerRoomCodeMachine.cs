b080aea621c781996a84dd6fa1d4881f5d53983e
Jonas Molsted <jonasmolsted@gmail.com>
2025-03-30 14:21:35 +0200

    lockerroom puzzle can be solved

--- Assets/Scripts/LockerRoomCodeMachine.cs
+++ Assets/Scripts/LockerRoomCodeMachine.cs
@@ -1,16 +1,36 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LockerRoomCodeMachine : MonoBehaviour
 {
+    public Image image;
+    public Button button;
+    public TMP_InputField inputField;
+
+    private bool puzzleSolved;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        button.onClick.AddListener( CheckLockerRoomCode );
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void CheckLockerRoomCode() {
+        if ( puzzleSolved ) return;
+
+        if ( inputField.text == "code" ) {
+            image.color = Color.green;
+            puzzleSolved = true;
+        } else {
+            image.color = Color.yellow;
+        }
         
     }
 }
