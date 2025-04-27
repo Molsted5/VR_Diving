af995ae50cfef284f2b437949de6386f2889f4cb
johanne <johanne@ornsovej.dk>
2025-04-27 13:09:29 +0200

    Fixed Washing station and Snorkel puzzles
    Puzzles should be up to date and working. Limited to one key pr. puzzle.

--- Assets/Scripts/HandInStation_controller.cs
+++ Assets/Scripts/HandInStation_controller.cs
@@ -5,45 +5,116 @@ using System.Collections;
 using UnityEngine.UI;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 public class HandInStation_controller : MonoBehaviour
 {
     public Transform KeySpawn;
     public GameObject KeyObject;
+    public Animator DoorAnimation;
+    public Button Activate;
+    public Image Indicator;
+    public TMP_Text InformText;
 
+    private bool KeyRecieved;
+    private Collider CurrentItem;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Activate.onClick.AddListener(OnActivationClick);
+        InformText.text = "Insert snorkel of of correct size";
+        Indicator.color = Color.grey;
+        KeyRecieved = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        StartCoroutine(CheckScale(other));
+        CurrentItem = other;
+    }
+
+    private void OnActivationClick()
+    {
+        if (KeyRecieved == false)
+        {
+            if (CurrentItem != null)
+            {
+                StartCoroutine(CheckScale(CurrentItem));
+            }
+            else
+            {
+                StartCoroutine(EmptyBox());
+            }
+        }
+        else
+        {
+            StartCoroutine(KeyIsRecieved());
+        }
+        
+    }
+
+    IEnumerator KeyIsRecieved()
+    {
+        InformText.text = "Puzzle already solved";
+        yield return new WaitForSeconds(3f);
+        InformText.text = "Insert snorkel of of correct size";
+    }
+    //Coroutine for if the box is empty. Start animation, wait 2 seconds, update text and color, wait 3 seconds then reset
+    IEnumerator EmptyBox()
+    {
+        DoorAnimation.SetBool("Unlocked", true);
+        yield return new WaitForSeconds(2f);
+        InformText.text = "No item in the box";
+        Indicator.color = Color.red;
+
+        yield return new WaitForSeconds(3f);
+
+        InformText.text = "Insert snorkel of of correct size";
+        Indicator.color = Color.grey;
+        DoorAnimation.SetBool("Unlocked", false);
     }
 
     IEnumerator CheckScale(Collider other)
     {
+        //Get the rootscale of entered object and start animation, then wait 2 seconds.
         float rootscale = other.transform.root.localScale.x;
+        DoorAnimation.SetBool("Unlocked", true);
 
         yield return new WaitForSeconds(2f);
-
-        if (rootscale >= 0.30f && rootscale <= 0.35)
+       
+        //Check scale of object
+        if (other.CompareTag("Snorkel"))
         {
-            Destroy(other.transform.root.gameObject);
-            Debug.Log("Scale is correct");
-            Instantiate(KeyObject, KeySpawn.position, KeySpawn.rotation);
-
+            if (rootscale >= 0.30f && rootscale <= 0.35f)
+            {
+                Destroy(other.transform.root.gameObject);
+                InformText.text = "Snorkel is within the recommended size";
+                Indicator.color = Color.green;
+                Instantiate(KeyObject, KeySpawn.position, KeySpawn.rotation);
+                KeyRecieved = true;
+                DoorAnimation.SetBool("Unlocked", false);
+            }
+            else
+            {
+                Destroy(other.transform.root.gameObject);
+                InformText.text = "Snorkel is not correct size";
+                Indicator.color = Color.red;
+                DoorAnimation.SetBool("Unlocked", false);
+            }
         }
         else
         {
-            Destroy(other.transform.root.gameObject);
-            Debug.Log("Scale not correct");
+            InformText.text = "Machine only accepts snorkels";
         }
+
+
+            //wait and reset text and button color
+            yield return new WaitForSeconds(4f);
+        Indicator.color = Color.grey;
+        InformText.text = "Insert snorkel of of correct size";
     }
 }
---
+++ Assets/Scripts/WashingStationController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WashingStationController : MonoBehaviour
+{
+    public Animator DoorAnimation;
+    public GameObject KeyObject;
+    public Transform KeySpawn;
+    public Button Activate;
+    public Image Indicator;
+    public TMP_Text InformText;
+
+    private bool KeyRecieved;
+    private Collider CurrentItem;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Activate.onClick.AddListener(OnActivationClick);
+        InformText.text = "Insert cleaning liquid for diving gear";
+        Indicator.color = Color.grey;
+        KeyRecieved = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CurrentItem = other;
+    }
+
+    private void OnActivationClick()
+    {
+        if(KeyRecieved == false)
+        {
+            if (CurrentItem != null)
+            {
+                StartCoroutine(CheckLiquid(CurrentItem));
+            }
+            else
+            {
+                StartCoroutine(EmptyBox());
+            }
+        }
+        else
+        {
+            StartCoroutine(KeyIsRecieved());
+        }
+        
+    }
+
+    IEnumerator KeyIsRecieved()
+    {
+        InformText.text = "Puzzle already solved";
+        yield return new WaitForSeconds(3f);
+        InformText.text = "Insert cleaning liquid for diving gear";
+    }
+    IEnumerator EmptyBox()
+    {
+        DoorAnimation.SetBool("Unlocked", true);
+        yield return new WaitForSeconds(2f);
+        InformText.text = "No item in the box";
+        Indicator.color = Color.red;
+
+        yield return new WaitForSeconds(3f);
+
+        InformText.text = "Insert cleaning liquid for diving gear";
+        Indicator.color = Color.grey;
+    }
+    IEnumerator CheckLiquid(Collider other)
+    {
+        DoorAnimation.SetBool("Unlocked", true);
+        yield return new WaitForSeconds(2f);
+
+        if (other.CompareTag("Water"))
+        {
+            Instantiate(KeyObject, KeySpawn.position, KeySpawn.rotation);
+            DoorAnimation.SetBool("Unlocked", false);
+            InformText.text = "Correct Liquid";
+            Indicator.color = Color.green;
+            KeyRecieved = true;
+        }
+        else
+        {
+            DoorAnimation.SetBool("Unlocked", false);
+            InformText.text = "Wrong Liquid";
+            Indicator.color = Color.red;
+        }
+
+        yield return new WaitForSeconds(4f);
+        InformText.text = "Insert cleaning liquid for diving gear";
+        Indicator.color = Color.grey;
+    }    
+   
+    
+}
